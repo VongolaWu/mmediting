b860042fab86ffb0948821f924f4a9510aff9745
VongolaWu <vongola8757@gmail.com>
2022-11-22 15:13:17 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -7,18 +7,18 @@
 # work_dirs/airnet/epoch_1500.pth
 
 # all in one
-# GPUS=4 GPUS_PER_NODE=4 ./tools/slurm_train.sh \
-# mm_lol \
-# airnet_o \
-# configs/airnet/airnet_cfg_allsets_air.py \
-# work_dirs/airnet \
-# --resume
+GPUS=4 GPUS_PER_NODE=4 ./tools/slurm_train.sh \
+mm_lol \
+airnet_o \
+configs/airnet/airnet_cfg_allsets_air.py \
+work_dirs/airnet \
+--resume
 
 # derain
-GPUS=2 GPUS_PER_NODE=2 ./tools/slurm_train.sh \
-mm_lol \
-air_rain \
-configs/airnet/airnet_cbde256drgn55643_2xb8_lr1e-3_1000E_rain.py \
+# GPUS=2 GPUS_PER_NODE=2 ./tools/slurm_train.sh \
+# mm_lol \
+# air_rain \
+# configs/airnet/airnet_cbde256drgn55643_2xb8_lr1e-3_1000E_rain.py \
 # --resume
 
 # dehaze
