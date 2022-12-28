967fa2adf1f538de3cffa4887b9eb7a8fb15e6bd
VongolaWu <vongola8757@gmail.com>
2022-12-28 11:36:33 +0800

    mmp.sh

--- mmp.sh
+++ mmp.sh
@@ -1,4 +1,5 @@
-PORT=21212 CUDA_VISIBLE_DEVICES=4,5,6,7 ./tools/dist_test.sh \
+GPUS=8 CPUS_PER_TASK=16 ./tools/slurm_test.sh \
+mm_lol \
+mmp-rnn \
 configs/mmp-rnn/mmp-rnn_adobe.py \
-checkpoint/mmp-rnn.pth \
-4
+checkpoint/mmp-rnn.pth
